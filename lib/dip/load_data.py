f389132665df2b16dbe1657bea988fc533b6a27b
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-10-29 13:26:05 +0900

    Add path.py

--- lib/dip/load_data.py
+++ lib/dip/load_data.py
@@ -5,18 +5,17 @@ import os
 from skimage.io import imread
 from sklearn.datasets import load_files
 
-
-_this_dir = os.path.dirname(os.path.abspath(__file__))
+from .path import *
 
 
 def load_raw_images():
-    container_path = os.path.join(_this_dir, '../data/raw_image')
+    container_path = os.path.join(DATA_DIR, 'raw_image')
     dataset = load_files(container_path=container_path, load_content=False)
     return dataset
 
 
 def load_mask_images():
-    container_path = os.path.join(_this_dir, '../data/mask_image')
+    container_path = os.path.join(DATA_DIR, 'mask_image')
     dataset = load_files(container_path=container_path, load_content=False)
     return dataset
 
---
+++ lib/dip/path.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+_this_dir = os.path.dirname(os.path.abspath(__file__))
+
+DATA_DIR=os.path.join(_this_dir, '../../data')
