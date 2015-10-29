b83bdf6e6381c91585c6400b533eb3b00bede18c
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-10-29 19:42:06 +0900

    Add comments

--- lib/dip/mask.py
+++ lib/dip/mask.py
@@ -5,15 +5,11 @@ import numpy as np
 
 
 def bounding_rect_of_mask(mask, negative=False):
+    """
+    @returns (min_x, max_x, min_y, max_y)
+    """
     if negative:
         mask = ~mask
     where = np.argwhere(mask)
     (y_start, x_start), (y_end, x_end) = where.min(0), where.max(0) + 1
-    return [x_start, x_end, y_start, y_end]
-
-
-def convert_masks_to_target(masks, negative=False):
-    target = []
-    for mask in masks:
-        target.append(bounding_rect_of_mask(mask, negative=negative))
-    return np.array(target)
+    return (x_start, x_end, y_start, y_end)
