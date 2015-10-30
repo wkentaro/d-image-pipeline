2bf563e4358aebb9a97f1bc7d110dfab07efd682
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-10-30 16:24:05 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dip',
+    version='0.1',
+    packages=find_packages(),
+    description='deep image pipeline',
+    long_description=open('README.rst').read(),
+    author='Kentaro Wada',
+    author_email='www.kentaro.wada@gmail.com',
+    url='http://github.com/wkentaro/d-image-pipeline',
+    install_requires=open('requirements.txt').readlines(),
+    license='MIT',
+    keywords='machine-learning',
+)
